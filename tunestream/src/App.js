ae4599b8c99f611b279133179805962482fa483f
Lisa Njoku <njokuleesah@gmail.com>
2024-10-12 15:10:14 +0000

    adding the util component, holding the list of songs

--- tunestream/src/App.js
+++ tunestream/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Song from './Components/Song';
 import Player from './Components/Player';
-import './styles/_app.scss'
+import './styles/_app.scss';
+import data from './util'
 
 function App() {
+//STATE
+const [song, setSong]  = useState(data());
+const [currentSong, setCurrentSong] = useState(song[4])
+
   return (
     <div className="App">
      <Song />
---
+++ tunestream/src/util.js
@@ -0,0 +1,97 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const chillHop = () => {
+    return [
+        {
+            name : 'Arbour',
+            artist : 'Psalm Trees',
+            cover : 'https://cms.chillhop.com/media/77775/squarel033693c01be6a3562891a473d0bd7c3af974fefa.jpg',
+            color : ['#877FBA', '#423543'],
+            audio : "https://stream.chillhop.com/mp3/73164",
+            id : uuidv4(),
+            active: true
+           },
+           {
+            name : 'Arbour',
+            artist : 'Psalm Trees',
+            cover : 'https://cms.chillhop.com/media/77775/squarel033693c01be6a3562891a473d0bd7c3af974fefa.jpg',
+            color : ['#877FBA', '#423543'],
+            audio : "https://stream.chillhop.com/mp3/73164",
+            id : uuidv4(),
+            active: false
+           },
+           {
+            name : 'Drive home',
+            artist : 'Shy cope',
+            cover : 'https://cms.chillhop.com/media/686/squareld491b424193bb9ca20c9a1f20419b5c167d37c96.jpg',
+            color : ['#374C83', '#202E3B'],
+            audio : "https://stream.chillhop.com/mp3/9257",
+            id : uuidv4(),
+            active: false
+           },
+           {
+            name : 'The Finish Line',
+            artist : 'Middle School',
+            cover : 'https://cms.chillhop.com/media/2354/squarel1b7f6e07967bf16feaa5628d843466d01a2141fe.jpg',
+            color : ['#FEDCDA', '##FE918B'],
+            audio : 'https://stream.chillhop.com/mp3/7902',
+            id : uuidv4(),
+            active: false
+           },
+           {
+            name : 'The Finish Line',
+            artist : 'Helplessly',
+            cover : 'https://cms.chillhop.com/media/2354/squarel1b7f6e07967bf16feaa5628d843466d01a2141fe.jpg',
+            color : ['#FEDCDA', '##FE918B'],
+            audio : 'https://stream.chillhop.com/mp3/7892',
+            id : uuidv4(),
+            active: false
+           },
+           {
+            name : 'Ruck P',
+            artist : 'Memories Pt.1',
+            cover : 'https://cms.chillhop.com/media/21795/squarel034f52eeaeceb144cca67b0930d705d32fc39e37.jpg',
+            color : ['#CC858B', '#6A6887'],
+            audio : 'https://stream.chillhop.com/mp3/21659',
+            id : uuidv4(),
+            active: false
+           },
+           {
+            name : 'Ruck P',
+            artist : 'Memories Pt.2',
+            cover : 'https://cms.chillhop.com/media/21795/squarel034f52eeaeceb144cca67b0930d705d32fc39e37.jpg',
+            color : ['#CC858B', '#6A6887'],
+            audio : "https://stream.chillhop.com/mp3/21660",
+            id : uuidv4(),
+            active: false
+           },
+           {
+            name : 'sugi.wa',
+            artist : 'After Dark',
+            cover : 'https://cms.chillhop.com/media/3775/squarelbe9e5dca9d4c9015c407a2d2590af8a0e5e278ce.jpg',
+            color : ['#14AA9B', '#F0FBC1'],
+            audio : "https://stream.chillhop.com/mp3/8105",
+            id : uuidv4(),
+            active: false
+           },
+           {
+            name : 'Space Cadet',
+            artist : 'Philanthrope',
+            cover : 'https://cms.chillhop.com/media/2644/squarelc85a5f6166a48001820ba3dc7e1da639281e00b3.jpg',
+            color : ['#C2BFAE', '#FAAB74'],
+            audio : "https://stream.chillhop.com/mp3/8841",
+            id : uuidv4(),
+            active: false
+           },
+           {
+            name : 'Lake',
+            artist : 'dryhope',
+            cover : 'https://cms.chillhop.com/media/2644/squarelc85a5f6166a48001820ba3dc7e1da639281e00b3.jpg',
+            color : ['#C2BFAE', '#FAAB74'],
+            audio : "https://stream.chillhop.com/mp3/8130",
+            id : uuidv4(),
+            active: false
+           },
+    ]
+}
+export default chillHop;
