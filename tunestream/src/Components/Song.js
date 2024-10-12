89984c6c910de912c8c317f9afe256619743cd42
Lisa Njoku <njokuleesah@gmail.com>
2024-10-12 14:00:08 +0000

    creating the  song  component

---
+++ tunestream/src/Components/Song.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Song =() =>{
+    return(
+        <div className='song-container'>
+        <hi>Song image</hi>
+        <h2>Artiste</h2>
+        <h3>Song title</h3>
+        </div>
+    )
+}
+
+export default Song;
